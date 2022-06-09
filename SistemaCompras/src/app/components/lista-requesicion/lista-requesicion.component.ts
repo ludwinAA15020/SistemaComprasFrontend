45523d69752ad0772919ed6dec941f190ea5895d
ludwinAA15020 <alfaroalonso45@gmail.com>
2022-06-08 22:09:20 -0600

    trabajando en requesicion

---
+++ SistemaCompras/src/app/components/lista-requesicion/lista-requesicion.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RequisicionService } from 'src/app/services/prequesicion.service';
+
+@Component({
+  selector: 'app-lista-requesicion',
+  templateUrl: './lista-requesicion.component.html',
+  styleUrls: ['./lista-requesicion.component.css']
+})
+export class ListaRequesicionComponent implements OnInit {
+  public listRequisicion: any[] = [];
+  public page: number = 0;
+
+  constructor(private router: Router, private _requisicionService: RequisicionService) { }
+
+  ngOnInit(): void {
+    this.obtenerRequisicion()
+  }
+  editarRequesicion() {
+    // console.log(producto);
+    this.router.navigate([`app-requesicion//${1}`]);
+  }
+  obtenerRequisicion() {
+    this._requisicionService.getListRequisicion().subscribe(data => {
+      this.listRequisicion = data;
+       console.log(this.listRequisicion);
+    }, error => {
+      console.log(error);
+    })
+
+
+}
+}
---
+++ SistemaCompras/src/app/components/requesicion/requesicion.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-requesicion',
+  templateUrl: './requesicion.component.html',
+  styleUrls: ['./requesicion.component.css']
+})
+export class RequesicionComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
